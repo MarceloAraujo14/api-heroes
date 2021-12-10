17961820c6457b21a1e3099bf60f526b09f06529
Marcelo Araujo <mbaraujo1406@gmail.com>
2021-12-09 22:15:24 -0300

    Add files via upload

---
+++ heroesapi/src/test/java/com/dio/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,52 @@
+package com.dio.heroesapi;
+
+import com.dio.heroesapi.repository.HeroesRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.dio.heroesapi.constants.HeroesConstant.HEROES_ENDPOINT_LOCAL;
+
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Autowired
+	WebTestClient webTestClient;
+
+	@Autowired
+	HeroesRepository heroesRepository;
+
+	@Test
+	public void getOneHeroById(){
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"), "3")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody();
+	}
+
+	@Test
+	public void getOneHeroNotFound(){
+		webTestClient.get().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"), "15")
+				.exchange()
+				.expectStatus().isNotFound();
+	}
+
+	@Test
+	public void deleteHero(){
+		webTestClient.delete().uri(HEROES_ENDPOINT_LOCAL.concat("/{id}"), "4")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isAccepted()
+				.expectBody(Void.class);
+	}
+
+}
